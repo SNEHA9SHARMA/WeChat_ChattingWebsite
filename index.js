07b0f7e46e82cff95650bc3637e391d784c87a7d
SNEHA9SHARMA <156566320+SNEHA9SHARMA@users.noreply.github.com>
2024-06-20 16:19:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//Node server which will handle socket server connections
+const io = require("socket.io")(8000);
+const users = {};
+
+ io.on('connection', socket=>{
+     socket.on('new-user-joined', name=>{
+         console.log("New user", name);
+         users[socket.id] = name;
+         socket.broadcast.emit('user-joined', name);
+     });
+io.engine.on("connection_error", (err) => {
+        console.log(err.req);      // the request object
+        console.log(err.code);     // the error code, for example 1
+        console.log(err.message);  // the error message, for example "Session ID unknown"
+        console.log(err.context);  // some additional error context
+      });
+     socket.on('send', message=>{
+         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+     });
+
+     socket.on('disconnect', message=>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+ })
